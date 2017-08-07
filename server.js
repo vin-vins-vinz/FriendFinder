c4cf5ac79e594429fb877f2d831e1fbe78b703d9
Liang <Nicky@Liangs-Air.fios-router.home>
2017-08-06 19:41:57 -0700

    setup

---
+++ server.js
@@ -0,0 +1,7 @@
+//Setup connection with heroku
+//install npm inpendencies: express, bodyparser, path
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
