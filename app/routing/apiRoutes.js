51a03dc73fa93ad966dd6a3b1ada7aa2cf2ea66e
Liang <Nicky@Liangs-Air.fios-router.home>
2017-08-06 22:43:44 -0700

    deploy 2

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,10 @@
 //Linking our routes to data
-var friendsData = require("../data/friendsData");
+var friendsData = require("../data/friends");
 
 //Routing data function
 module.exports = function(app){
-    //API POST requests, to show all possible results of friends in database.
-    app.post("/api/friends", function(req, res){
+    //API GET request, to show all possible results of friends in database when user visit this API link.
+    app.get("/api/friends", function(req, res){
         res.json(friendsData);
-        console.log(friendsData);
     });
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,7 @@ var path = require("path");
 module.exports = function(app){
 //HTML GET requests
 //Set Home page as default route
-    app.use(function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 //Survey Questions page
