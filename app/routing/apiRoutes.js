6af61a8d82a4e7180fa512574a6772cb057aaa47
Liang <Nicky@Liangs-Air.fios-router.home>
2017-08-08 00:03:18 -0700

    first commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,4 +7,9 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendsData);
     });
+
+    //by getting User's data filled out in survey html page, create an object.
+    //use a for loop to loop through friendsData array to compare and find matching result.
+    //use post method, to push this new user's data into the friendsData object. 
+    //app.post("/api/friends", function(req,res){});
 };
